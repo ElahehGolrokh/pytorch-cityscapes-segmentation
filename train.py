1728b5bef83819121a06c3f8e7c17dab28fe2513
Eli69Pro <elaheh.golrokh@gmail.com>
2025-11-25 10:10:29 +0330

    adjust train with new data loader

--- train.py
+++ train.py
@@ -2,9 +2,8 @@ import os
 import argparse
 
 from omegaconf import OmegaConf
-from pathlib import Path
 
-from src.data_loader import DataGenerator
+from src.data_loader import DataGenerator, create_data_paths
 from src.training import Trainer
 
 
@@ -19,17 +18,19 @@ args = parser.parse_args()
 def main(config_path):
     config = OmegaConf.load(config_path)
     batch_size = config.training.batch_size
+
     train_dir = os.path.join('data', 'train', 'train')
     val_dir = os.path.join('data', 'valid', 'valid')
 
-    train_loader = DataGenerator(train_dir,
-                                 phase="train",
+    train_paths = create_data_paths(train_dir)
+    val_paths = create_data_paths(val_dir)
+
+    train_loader = DataGenerator(phase="train",
                                  batch_size=batch_size,
-                                 shuffle=True).load_data()
-    val_loader = DataGenerator(val_dir,
-                               phase="val",
-                               batch_size=len(os.listdir(val_dir)),
-                               shuffle=False).load_data()
+                                 shuffle=True).load_data(train_paths)
+    val_loader = DataGenerator(phase="val",
+                               batch_size=len(val_paths),
+                               shuffle=False).load_data(val_paths)
 
     trainer = Trainer(
         config=config,
